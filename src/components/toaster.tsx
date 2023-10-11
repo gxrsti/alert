ad396af8837517fa7ecfb1bcad579bc2dcbdc0e0
Gersti <patrick.gerstacker@gmail.com>
2023-10-11 21:33:36 +0200

    refactor: refactoring

--- src/components/toaster.tsx
+++ src/components/toaster.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { IToast, ToasterProps } from '../core/types';
 import { Store } from '../core/store';
 import { Toast } from './toast';
@@ -15,8 +15,8 @@ const Toaster = ({ position = 'bottom-right', style, className }: ToasterProps) 
 
   return (
     <section style={{ position: 'absolute', bottom: 20, right: 20, display: 'flex', flexDirection: 'column', gap: 5 }}>
-      {toasts.map((toast) => (
-        <Toast toast={toast} remove={removeToast} />
+      {toasts.map((toast, index) => (
+        <Toast key={index} toast={toast} remove={removeToast} />
       ))}
     </section>
   );
