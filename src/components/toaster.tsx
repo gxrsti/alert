f535c8ead517320c27951738c19a7c966e27e38c
Gersti <patrick.gerstacker@gmail.com>
2023-10-11 21:14:46 +0200

    refactor: styling

--- src/components/toaster.tsx
+++ src/components/toaster.tsx
@@ -11,9 +11,25 @@ const Toaster = ({ position = 'bottom-right', style, className }: ToasterProps) 
   });
 
   return (
-    <section>
+    <section style={{ position: 'absolute', bottom: 20, right: 20, display: 'flex', flexDirection: 'column', gap: 5 }}>
       {toasts.map((toast, index) => (
-        <p key={index}>{toast.title}</p>
+        <div
+          style={{
+            paddingTop: '0.5rem',
+            paddingBottom: '0.5rem',
+            paddingLeft: '1rem',
+            paddingRight: '1rem',
+            borderRadius: '0.375rem',
+            backgroundColor: 'rgb(243, 244, 246)',
+            fontWeight: 500,
+            fontSize: '0.875rem',
+            lineHeight: '1.25rem',
+            color: 'rgb(17, 24, 39)',
+          }}
+          key={index}
+        >
+          {toast.title}
+        </div>
       ))}
     </section>
   );
