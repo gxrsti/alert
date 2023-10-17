c1cfea9c3415cf0dc829ba87c7ef1854da47bc95
Aditya Nikam <105343290+AdityaNik@users.noreply.github.com>
2023-10-17 13:30:22 +0530

    added select box for changing position of Toaster (#14)
    * added select box for changing position of Toaster
    
    * Update website/app/page.tsx
    
    applied suggested changes
    
    Co-authored-by: Gersti <88879471+gxrsti@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Gersti <88879471+gxrsti@users.noreply.github.com>

--- website/app/page.tsx
+++ website/app/page.tsx
@@ -1,12 +1,44 @@
 'use client';
 import { Button } from '@/components/ui/button';
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
 import { Toaster, toast } from 'alert';
+import { useState } from 'react';
+
+type ToasterPosition = 'top-left' | 'top-center' | 'top-right' | 'bottom-left' | 'bottom-center' | 'bottom-right';
 
 export default function App() {
+  const [position, setPosition] = useState<ToasterPosition>('bottom-right');
+  
   return (
     <div className="w-screen h-screen flex justify-center items-center">
-      <Toaster />
+      <Toaster position={position}/>
       <div className="flex gap-4">
+        <Select value={position} onValueChange={(s) => {
+          setPosition(s as ToasterPosition);
+        }}>
+          <SelectTrigger style={{color: 'black'}} className="w-[180px] h-[auto]">
+            <SelectValue placeholder="Select Position" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              <SelectLabel>Positions</SelectLabel>
+              <SelectItem value='top-left'>top-left</SelectItem>
+              <SelectItem value='top-center'>top-center</SelectItem>
+              <SelectItem value='top-right'>top-right</SelectItem>
+              <SelectItem value='bottom-left'>bottom-left</SelectItem>
+              <SelectItem value='bottom-center'>bottom-center</SelectItem>
+              <SelectItem value='bottom-right'>bottom-right</SelectItem>
+            </SelectGroup>
+          </SelectContent>
+        </Select>
         <Button variant="secondary" onClick={() => toast('This is a default toast')}>
           Default
         </Button>
